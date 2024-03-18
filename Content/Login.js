630e06e2f0a62dc274cbd7956aedf89ac405214f
Shweta <jadhavshweta077@gmail.com>
2024-03-18 16:59:15 +0530

    login

---
+++ Content/Login.js
@@ -0,0 +1,10 @@
+// LoginScreen.js
+import { View, Text, StyleSheet } from "react-native";
+import React from "react";
+
+const Login =() => {
+    return <View style ={Styles.mainContainer}></View>
+
+    
+};
+export default Login;
